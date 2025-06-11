89ad268ab9b951cc20adf8a60dc67e310fbeb76b
KMV531 <koladjamomovinnybrayan3@gmail.com>
2025-06-11 14:45:21 +0200

    Fixed element spacing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="container mx-auto px-5 mt-10">
+    <footer className="container mx-auto px-5 mt-32">
       <div
         className="
           grid 
